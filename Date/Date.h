a3c70f2a22086ecdec28fcadba3c157ffb26a253
SenDen <senden1998@gmail.com>
2018-12-19 13:53:50 +0200

    Few tests added

--- Date/Date.h
+++ Date/Date.h
@@ -1,13 +1,15 @@
 #pragma once
 
 struct Date {
-	unsigned short int day;
-	unsigned short int month;
-	long int year;
+	int32_t y;
+	uint8_t m;
+	uint8_t d;
 };
 
+/*
 void print(Date date);
 bool isEqual(Date d1, Date d2);
 void logEqualTest(Date d1, Date d2);
+*/
 
 Date nextDay(Date date);
--- Date/Source.cpp
+++ Date/Source.cpp
@@ -4,6 +4,7 @@
 
 int main()
 {
+	/*
 	Date d11 = { 19, 12, 2018 };
 	Date d12 = { 20, 12, 2018 };
 	logEqualTest(nextDay(d11), d12);
@@ -19,6 +20,23 @@ int main()
 	Date d41 = { 28, 2, 2000 };
 	Date d42 = { 29, 2, 2000 };
 	logEqualTest(nextDay(d41), d42);
+	*/
+
+
+	// Steep to next day
+	Date d1 = { 2018, 12, 19 };
+	Date d2 = nextDay(d1);
+	//DO_TEST(d2 == Date(2018, 12, 20))
+
+	// Steep to next month
+	Date d1 = { 2018, 01, 31 };
+	Date d2 = nextDay(d1);
+	//DO_TEST(d2 == Date(2018, 02, 01))
+
+	// Steep to next year
+	Date d1 = { 2018, 12, 31 };
+	Date d2 = nextDay(d1);
+	//DO_TEST(d2 == Date(2019, 01, 01))
 
 	return 0;
 }
